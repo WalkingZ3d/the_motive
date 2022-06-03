816dbac76e5dc8a8181ebbacf3660310cce0eec4
kharileigh <kharileighmiller@gmail.com>
2022-06-03 11:55:18 +0100

    ADDED REACT FILE STRUCTURE

---
+++ client/src/pages/LandingPage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const LandingPage = () => {
+    return (
+        <>
+        <div className='main-container'>
+
+            <div className='typewriter'>
+            
+                <h1>THE MOTIVE</h1>
+            
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default LandingPage;
